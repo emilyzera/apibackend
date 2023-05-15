6694e187b0c0573fa06287cce7d64b3a61f93259
emily wust <emily.ufc18@gmail.com>
2023-05-15 00:16:54 -0300

    email

--- src/database/users.ts
+++ src/database/users.ts
@@ -1,7 +1,7 @@
 import { Usuario } from "../models/usuario.model";
 
 export const Users: Usuario[] = [
-    new Usuario("emily.ufc18@gmail.com", "emily"),
+    new Usuario("emily.ufc18@gmail.com", "emilywust"),
     new Usuario("emily@hotmail.com","azul"),
     new Usuario("wellington@gmail.com", "marinha"), 
 ];
